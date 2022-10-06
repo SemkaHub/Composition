3b88a9d2a20f4fd147b01bc28eaa37bf233cb1f1
Семён Голубев <41947359+SemkaHub@users.noreply.github.com>
2022-10-06 23:09:00 +1200

    Prepare resources

--- app/src/main/java/com/example/android/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/example/android/composition/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.android.composition.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.android.composition.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
