4207eedcc204b43bd815a4e1731977baa2c60162
Семён Голубев <41947359+SemkaHub@users.noreply.github.com>
2022-10-07 17:44:38 +1200

    Add View Binding

--- app/src/main/java/com/example/android/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/android/composition/presentation/WelcomeFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.android.composition.R
+import com.example.android.composition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("WelcomeFragment == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
